35187c0a3bb2b9a9eba12f087b95272d2e73c826
Maciej Mezyk <mcj.mezyk@gmail.com>
2025-01-23 23:50:08 +0100

    Add calendar

--- src/app/calendar/appointment-dialog/appointment-dialog.component.ts
+++ src/app/calendar/appointment-dialog/appointment-dialog.component.ts
@@ -1,7 +1,9 @@
+import { AuthService } from './../../shared/auth.service';
 import { Component, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Appointment } from '../appointment.model';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CalendarService } from '../calendar.service';
 
 @Component({
   selector: 'app-appointment-dialog',
@@ -13,7 +15,9 @@ export class AppointmentDialogComponent {
 
   constructor(
     private fb: FormBuilder,
+    private calendarService: CalendarService,
     private dialogRef: MatDialogRef<AppointmentDialogComponent>,
+    private authService: AuthService,
     @Inject(MAT_DIALOG_DATA)
     public data: { date: Date; timeSlot: string; doctorId: string }
   ) {
@@ -28,27 +32,35 @@ export class AppointmentDialogComponent {
 
   onSubmit() {
     if (this.appointmentForm.valid) {
-      const formValue = this.appointmentForm.value;
-      const [hours, minutes] = this.data.timeSlot.split(':').map(Number);
-      const startTime = new Date(this.data.date);
-      startTime.setHours(hours, minutes, 0, 0);
+      this.authService.getCurrentUser().then((user) => {
+        if (!user) return;
 
-      const endTime = new Date(startTime);
-      endTime.setMinutes(endTime.getMinutes() + 30);
+        const formValue = this.appointmentForm.value;
+        const [hours, minutes] = this.data.timeSlot.split(':').map(Number);
+        const startTime = new Date(this.data.date);
+        startTime.setHours(hours, minutes, 0, 0);
 
-      const appointment: Omit<Appointment, 'id'> = {
-        doctorId: this.data.doctorId,
-        startTime,
-        endTime,
-        status: 'zajęta',
-        type: formValue.type,
-        patientName: formValue.patientName,
-        patientGender: formValue.patientGender,
-        patientAge: formValue.patientAge,
-        notes: formValue.notes,
-      };
+        const endTime = new Date(startTime);
+        endTime.setMinutes(endTime.getMinutes() + 30);
 
-      this.dialogRef.close(appointment);
+        const appointment: Omit<Appointment, 'id'> = {
+          doctorId: this.data.doctorId,
+          patientId: user.uid,
+          startTime,
+          endTime,
+          status: 'zajęta',
+          type: formValue.type,
+          patientName: formValue.patientName,
+          patientGender: formValue.patientGender,
+          patientAge: formValue.patientAge,
+          notes: formValue.notes,
+        };
+
+        this.calendarService
+          .createAppointment(appointment)
+          .then(() => this.dialogRef.close(true));
+      });
+      this.dialogRef.close(true);
     }
   }
 
