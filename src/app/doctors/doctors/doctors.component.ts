f7ea0e1e6ffbbbe07adf4ec0d18970d5cac7abea
Maciej Mezyk <mcj.mezyk@gmail.com>
2025-01-29 15:36:23 +0100

    Add doctors component

--- src/app/doctors/doctors/doctors.component.ts
+++ src/app/doctors/doctors/doctors.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { User } from '../../shared/user';
+import { AdminService } from '../../admin/admin.service';
 
 @Component({
   selector: 'app-doctors',
-  templateUrl: './doctors.component.html',
-  styleUrl: './doctors.component.css'
-})
-export class DoctorsComponent {
+  templateUrl: 'doctors.component.html',
+  styles: [
+    `
+      .doctor-card {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+      }
 
+      .doctor-card mat-card-content {
+        flex-grow: 1;
+      }
+
+      .doctor-card mat-card-actions {
+        padding: 16px;
+      }
+    `,
+  ],
+})
+export class DoctorsComponent implements OnInit {
+  doctors$: Observable<User[]>;
+
+  constructor(private adminService: AdminService, private router: Router) {
+    this.doctors$ = this.adminService.getDoctors();
+  }
+
+  ngOnInit(): void {}
 }
